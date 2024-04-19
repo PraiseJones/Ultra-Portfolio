11c36462530a29ce07259aefdb945d15e5e8a362
PraiseJones <praisejones18@gmail.com>
2024-04-19 01:38:06 +0100

    updated nav

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,15 +1,40 @@
-const menuButton = document.getElementById('menubtn');
-const closeButton = document.getElementById('closebtn');
-const menuSection = document.getElementById('menu-sect');
-const navLinks = document.querySelectorAll('#menu-sect ul li');
+const tl = gsap.timeline({ paused: true });
 
-// Function to toggle menu visibility
-function toggleMenu(e) {
+const animateMenuOpen = () => {
+  tl.to("#menu-sect", {
+    autoAlpha: 1,
+    delay: 0.1,
+    ease: "power1.in",
+  })
+  .from("#menu-sect li", {
+    opacity: 0,
+    y: 20,
+    duration: 0.3,
+    stagger: 0.2,
+  });
+};
+
+const openMenu = () => {
+  const menuButton = document.getElementById("menubtn");
+  const closeButton = document.getElementById("closebtn");
+  const menuSection = document.getElementById("menu-sect");
+
+  function toggleMenu(e) {
     e.preventDefault();
-  menuSection.classList.toggle('hidden');
+    menuSection.classList.toggle("hidden");
 
-}
+    const isOpen = !menuSection.classList.contains("hidden");
+    if (isOpen) {
+      animateMenuOpen();
+      tl.play();
+    } else {
+    //   tl.reverse();
+    }
+  }
 
-// Event listeners for menu button and close button
-menuButton.addEventListener('click', toggleMenu);
-closeButton.addEventListener('click', toggleMenu);
+  menuButton.addEventListener("click", toggleMenu);
+  closeButton.addEventListener("click", toggleMenu);
+};
+
+openMenu();
+
