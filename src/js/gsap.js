c846a59f41d489de4c0170bcc0f5e1e5c05b085d
PraiseJones <praisejones18@gmail.com>
2024-04-19 15:30:39 +0100

    animation

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,18 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tl = gsap.timeline({ defaults: { duration: 1 } });
+  
+  tl.from("#navbar", {
+    opacity: 0,
+    y: "-100",
+    ease: "power1.out",
+  })
+  .from('#ultra-txt', {
+    opacity:  0,
+    duration: 0.8,
+    y: 20,
+    stagger:0.2
+  })
+  .from('#ultra-txt2', {
+    opacity:  0,
+    duration: 0.8,
+    y: 20,
+    stagger:0.1
+  });;
+});
+
 const tl = gsap.timeline({ paused: true });
 
 tl.to("#menu-sect", {
   autoAlpha: 1,
   delay: 0.1,
   ease: "power1.in",
-}).from("#menu-sect li", {
-  opacity: 0,
-  y: 20,
-  duration: 0.3,
-  stagger: 0.2,
-});
+})
+  .from("#menu-sect li", {
+    opacity: 0,
+    y: 20,
+    duration: 0.3,
+    stagger: 0.2,
+  })
+  .to("#sec-sep", {
+    autoAlpha: 1,
+    duration: 1.5,
+    width: "100%",
+    ease: "power4.inOut",
+  });
 
-const menuButton = document.getElementById("menubtn");
-const closeButton = document.getElementById("closebtn");
+const menubtn = document.getElementById("menubtn");
+const closebtn = document.getElementById("closebtn");
 const menuSection = document.getElementById("menu-sect");
 
 function toggleMenu(e) {
@@ -27,7 +56,5 @@ function toggleMenu(e) {
   }
 }
 
-menuButton.addEventListener("click", toggleMenu);
-closeButton.addEventListener("click", toggleMenu);
-
-
+menubtn.addEventListener("click", toggleMenu);
+closebtn.addEventListener("click", toggleMenu);
